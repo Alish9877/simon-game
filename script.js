56675895831b257836066b197f97cbcb37e474e0
alish9877 <alshaikhali302@gmail.com>
2024-11-19 15:01:13 +0300

    css changes

--- script.js
+++ script.js
@@ -1,50 +1,49 @@
-const colorArray = ['red', 'blue', 'yellow', 'green'];
+const colorArray = ['red', 'blue', 'yellow', 'green']
 
-const redButton = document.querySelector('.red');
-const blueButton = document.querySelector('.blue');
-const yellowButton = document.querySelector('.yellow');
-const greenButton = document.querySelector('.green');
-const startButton = document.querySelector('.start-button');
-const quitButton = document.querySelector('.quit-button');
-
+const redButton = document.querySelector('.red')
+const blueButton = document.querySelector('.blue')
+const yellowButton = document.querySelector('.yellow')
+const greenButton = document.querySelector('.green')
+const startButton = document.querySelector('.start-button')
+const quitButton = document.querySelector('.quit-button')
 const statusms = document.querySelector('.status')
 const level = document.querySelector('.level')
 
-let sequenceColor = [];
+let sequenceColor = []
 let score = 0;
 let isGameActive = false;
-let userSequence = [];
+let userSequence = []
 
 const startGame = () => {
-  isGameActive = true;
-  userSequence = [];
-  sequenceColor = [];
-  score = 0;
-  levelScore();
-  presentSequence();
-};
+  isGameActive = true
+  userSequence = []
+  sequenceColor = []
+  score = 0
+  levelScore()
+  presentSequence()
+}
 
 const levelScore = () => {
-  score++;
-  userSequence = [];
-  const color = colorArray[Math.floor(Math.random() * 4)];
-  sequenceColor.push(color);
+  score++
+  userSequence = []
+  const color = colorArray[Math.floor(Math.random() * 4)]
+  sequenceColor.push(color)
 level.textContent = `level: ${score}`
-};
+}
 
 // const playaudio = (color) => {
-//   const realpath = sounds/`${colorArray}`.wav;
-//   const audio = new Audio(realpath);
-//   audio.play();
-// };
+//   const realpath = `./sounds/${color}.mp3`
+//   const audio = new Audio(realpath)
+//   audio.play()
+// }
 
 const flashEffect = (color) => {
-  const button = document.querySelector(`.${color}`);
-  button.style.backgroundColor = getFlashColor(color);
+  const button = document.querySelector(`.${color}`)
+  button.style.backgroundColor = getFlashColor(color)
 
   setTimeout(() => {
-    button.style.backgroundColor = '';
-  }, 300);
+    button.style.backgroundColor = ''
+  }, 300)
 };
 
 const getFlashColor = (color) => {
@@ -54,51 +53,51 @@ const getFlashColor = (color) => {
     yellow: 'lightyellow',
     green: 'lightgreen',
   };
-  return flashColors[color];
+  return flashColors[color]
 };
 
 const presentSequence = () => {
-  let i = 0;
+  let i = 0
   const interval = setInterval(() => {
-    // playaudio(sequenceColor[i]);
-    flashEffect(sequenceColor[i]);
-    i++;
+    // playaudio(sequenceColor[i])
+    flashEffect(sequenceColor[i])
+    i++
     if (i >= sequenceColor.length) {
-      clearInterval(interval);
+      clearInterval(interval)
     }
-  }, 600);
-};
+  }, 600)
+}
 
 
 const checkinput = () => {
-  const lastIndex = userSequence.length - 1;
+  const lastIndex = userSequence.length - 1
   if (userSequence[lastIndex] !== sequenceColor[lastIndex]) {
-     statusms.textContent = `Game Over! Your score: ${score}`
-    endGame();
+    statusms.textContent = `Game Over! Your score: ${score}`
+    endGame()
   } else if (userSequence.length === sequenceColor.length) {
-    levelScore();
-    setTimeout(presentSequence, 1000);
+    levelScore()
+    setTimeout(presentSequence, 1000)
   }
-};
+}
 
 
 const handleClick = (event) => {
   const color = event.target.classList[0];
   if (isGameActive) {
-    userSequence.push(color);
-    // playaudio(color);
-    flashEffect(color);
-    checkinput();
+    userSequence.push(color)
+    // playaudio(color)
+    flashEffect(color)
+    checkinput()
   }
-};
+}
 
 const endGame = () => {
-  isGameActive = false;
-  // alert(`Game Over! Your score: ${score}`);
-};
+  isGameActive = false
+  alert(`Game Over! Your score: ${score}`)
+}
 
-redButton.addEventListener('click', handleClick);
-blueButton.addEventListener('click', handleClick);
-yellowButton.addEventListener('click', handleClick);
-greenButton.addEventListener('click', handleClick);
-startButton.addEventListener('click', startGame);
+redButton.addEventListener('click', handleClick)
+blueButton.addEventListener('click', handleClick)
+yellowButton.addEventListener('click', handleClick)
+greenButton.addEventListener('click', handleClick)
+startButton.addEventListener('click', startGame)
